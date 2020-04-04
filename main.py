37f1c56ee1fa6a4155ac1a6229db35cc98b81125
Mariusz Milewski <mariuszmaxmilewski@gmail.com>
2020-04-04 16:41:36 +0200

    Task 4 attempt 1

--- main.py
+++ main.py
@@ -27,37 +27,37 @@ umieć posługiwać JSONami.
 Na tym etapie kursu nie bawimy się w bazy danych. Być może spostrzeżesz bardzo ciekawe zachowanie ;-)
 
 PS 2 Przed uruchomieniem testów, należy zrestartować swoją aplikację, żeby licznik na początku miał wartość 0!
+
+Zadanie 4
+Stwórz ścieżkę `/patient/{pk}`, która przyjmuje request w metodą GET.
+
+pk, powinien być liczbą. Najlepiej intem.
+
+W przypadku znalezienia takiego pacjenta, odpowiedź powinna wyglądać tak:
+`{"name": "NAME", "surename": "SURENAME"}`
+
+W przypadku nieznalezienia należy zwrócić odpowiedni kod http:
+https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+
+PS 1 Podobnie jak w poprzednim zadaniu, znajdź miejsce w aplikacji, gdzie można zapisać pacjenta
+
+PS 2 Zmodyfikuj endpoint POST `/patient`, tak aby zachował przesłane dane
+
+PS 3 Pamiętaj, aby liczyć pacjentów od 0!
 """
-from fastapi import FastAPI
+
+
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 app = FastAPI()
-app.num = -1
-l1 = []
+app.num = 0
+app.count = -1
+patlist = []
 
 
 @app.get("/")
 def root():
 	return {"message": "Hello World during the coronavirus pandemic!"}
-
-
-@app.get("/method")
-def return_get():
-	return {"method": "GET"}
-
-
-@app.post("/method")
-def return_post():
-	return {"method": "POST"}
-
-
-@app.put("/method")
-def return_put():
-	return {"method": "PUT"}
-
-
-@app.delete("/method")
-def return_delete():
-	return {"method": "DELETE"}
 
 
 @app.get("/num")
@@ -68,7 +68,7 @@ def num():
 
 @app.get("/patient")
 def l_patients():
-	return {"lista": l1}
+	return {"lista": patlist}
 
 
 class Request(BaseModel):
@@ -77,13 +77,21 @@ class Request(BaseModel):
 
 
 class Respond(BaseModel):
-	id: int
+	id: int = None
 	patient: dict
 
 
 @app.post("/patient", response_model=Respond)
 def new_patient(data: Request):
 	if data:
-		l1.append(data.dict())
-		app.num +=1
-	return Respond(id = app.num, patient = data.dict())
+		patlist.append(data.dict())
+		app.count +=1
+	return Respond(id = app.count, patient = data.dict())
+
+
+@app.get("/patient/{pk}")
+def get_patient(pk: int):
+	if pk in range(len(patlist)):
+		return patlist[pk]
+	else:
+		raise HTTPException(status_code=204, detail="No Content")
