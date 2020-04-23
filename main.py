20ad7c74aea086d238ec85389e3de8e5c2cfcec3
Mariusz Milewski <mariuszmaxmilewski@gmail.com>
2020-04-23 22:37:02 +0200

    HW3 Task2 Attempt1

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Cookie, Response, Depends
+from hashlib import sha256
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
+from starlette.responses import RedirectResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+
 app = FastAPI()
 app.num = 0
 app.count = -1
+app.users = {"trudnY": "PaC13Nt", "admin": "admin"}
+app.secret = "secret"
+app.tokens = []
 patlist = []
+
+security = HTTPBasic()
 
 
 @app.get("/")
@@ -12,10 +22,28 @@ def root():
 	return {"message": "Hello World during the coronavirus pandemic!"}
 
 
+@app.post("/welcome")
 @app.get("/welcome")
 def welcome_to_the_jungle():
 	return {"message": "welcome to the jungle! We have funny games!"}
 
+
+@app.post("/login")
+def login_to_app(user: str, passw: str, response: Response):
+	if user in app.users and passw == app.users[user]:
+		s_token = sha256(bytes(f"{user}{passw}{app.secret}", encoding='utf8')).hexdigest()
+		app.tokens += s_token
+		response.set_cookie(key="session_token",value=s_token)
+		response = RedirectResponse(url='/welcome')
+		print('logged in')
+		return response
+	else:
+		return "username or password is incorrect"
+
+
+@app.get("/users/me")
+def read_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    return {"username": credentials.username, "password": credentials.password}
 
 @app.get("/num")
 def num():
