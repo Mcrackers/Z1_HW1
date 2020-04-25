96866f8e09f53d12182a45ed5e86bdbe68f58dcd
Mariusz Milewski <mariuszmaxmilewski@gmail.com>
2020-04-25 12:02:03 +0200

    HW3 Task2 Attempt6

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ def login_to_app(response: Response, credentials: HTTPBasicCredentials=Depends(H
 		app.tokens += s_token
 		response.set_cookie(key="session_token",value=s_token)
 		response.status_code = 307
+		response.headers['Location'] = "/welcome"
 		RedirectResponse(url='/welcome')
 	else:
 		raise HTTPException(status_code=401, detail="Niepoprawny login lub haslo")
