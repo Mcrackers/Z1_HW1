d90b86b75f4f0ec8502c9f99451d386dbd7f7f08
Mariusz Milewski <mariuszmaxmilewski@gmail.com>
2020-04-26 20:20:09 +0200

    HW3 Task5 Attempt3

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
-app.num = 0
-app.count = 0
+app.patient_id = 0
 app.users = {"trudnY": "PaC13Nt", "admin": "admin"}
 app.secret = "secret"
 app.tokens = []
-patlist = []
+app.patient_list = {}
 
 template = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 def root():
@@ -26,7 +26,7 @@ def root():
 def welcome_to_the_jungle(request: Request, s_token = Cookie(None)):
 	if s_token not in app.tokens:
 		raise HTTPException(status_code=401, detail="dostęp wzbroniony")
-	return template.TemplateResponse("template1.html", {"request": request, "user": "trudnY"})
+	return template.TemplateResponse("item.html", {"request": request, "user": "trudnY"})
 
 
 @app.post("/login")
@@ -44,7 +44,7 @@ def login_to_app(response: Response, credentials: HTTPBasicCredentials = Depends
 
 
 @app.post("/logout")
-def byebye(response: Response):
+def bye_bye(response: Response):
 	response.delete_cookie(key="session_token",path="/")
 	response.status_code = 307
 	RedirectResponse(url='/')
@@ -53,73 +53,36 @@ def byebye(response: Response):
 
 
 @app.post("/patient")
-def add_patient(response: Response, name: str, surename: str, id = app.count, s_token = Cookie(None)):
+def add_patient(response: Response, name: str, surename: str, s_token = Cookie(None)):
 	if s_token not in app.tokens:
 		raise HTTPException(status_code=401, detail="dostęp wzbroniony")
-	patlist.append({name: surename})
-	app.count +=1
+	app.patient_id +=1
+	app.patient_list[app.patient_id] = {name:surename}
 	response.status_code = 307
-	response.headers['Location'] = f"/patient/{id}"
-	RedirectResponse(url=f'/patient/{id}')
-	return JSONResponse(patlist[int(id)])
+	response.headers['Location'] = f"/patient/{app.patient_id}"
+	RedirectResponse(url=f'/patient/{app.patient_id}')
+	return JSONResponse(app.patient_list[app.patient_id])
 
 
 @app.get("/patient")
 def show_patients(s_token = Cookie(None)):
 	if s_token not in app.tokens:
 		raise HTTPException(status_code=401, detail="dostęp wzbroniony")
-	return JSONResponse(["id_"+str(i)+":"+str(patlist[i]) for i in range(len(patlist))])
+	return JSONResponse(app.patient_list)
 
 
 @app.get("/patient/{id}")
-def show_patients(id: int ,s_token = Cookie(None)):
+def show_patients(id: int, s_token = Cookie(None)):
 	if s_token not in app.tokens:
 		raise HTTPException(status_code=401, detail="dostęp wzbroniony")
-	return JSONResponse(patlist[id])
+	return JSONResponse(app.patient_list[id])
+
 
 @app.delete("/patient/{id}")
 def show_patients(response: Response, id: int, s_token = Cookie(None)):
 	if s_token not in app.tokens:
 		raise HTTPException(status_code=401, detail="dostęp wzbroniony")
-	patlist.pop(id)
+	app.patient_list.pop(id)
 	response.status_code = 307
 	response.headers['Location'] = f"/patient"
 	RedirectResponse(url=f'/patient')
-
-@app.get("/num")
-def num():
-	app.num += 1
-	return app.num
-
-
-'''
-@app.get("/patient")
-def l_patients():
-	return {"lista": patlist}
-
-
-class Request(BaseModel):
-	name: str
-	surename: str
-
-
-class Respond(BaseModel):
-	id: int = None
-	patient: dict
-
-
-@app.post("/patient", response_model=Respond)
-def new_patient(data: Request):
-	if data:
-		patlist.append(data.dict())
-		app.count +=1
-	return Respond(id = app.count, patient = data.dict())
-
-
-@app.get("/patient/{pk}")
-def get_patient(pk: int):
-	if pk in range(len(patlist)):
-		return Request(name = patlist[pk]["name"], surename = patlist[pk]["surename"])
-	else:
-		return JSONResponse(status_code=204, content={})
-'''
