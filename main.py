4c3d8aca00a1f3035df57e448ddd156e8cfa6bab
Mariusz Milewski <mariuszmaxmilewski@gmail.com>
2020-04-27 10:53:48 +0200

    HW3 Task5 Attempt5

--- main.py
+++ main.py
@@ -16,34 +16,34 @@ app.patient_list = {}
 template = Jinja2Templates(directory="templates")
 
 
-def check_stupid_cookie(s_token: str = Cookie(None)):
-	print(s_token)
-	print(app.tokens)
-	if s_token not in app.tokens:
-		s_token = None
-	return s_token
-
-
 @app.get("/")
 def root():
 	return {"message": "Hello World during the coronavirus pandemic!"}
 
 
+def check_stupid_cookie(session_token: str = Cookie(None)):
+	print(session_token)
+	print(app.tokens)
+	if session_token not in app.tokens:
+		session_token = None
+	return session_token
+
+
 @app.get("/welcome")
-def welcome_to_the_jungle(request: Request, s_token: str = Depends(check_stupid_cookie)):
-	if s_token is None:
+def welcome_to_the_jungle(request: Request, session_token: str = Depends(check_stupid_cookie)):
+	if session_token is None:
 		raise HTTPException(status_code=401, detail="dostęp wzbroniony")
-	user = app.tokens[s_token]
+	user = app.tokens[session_token]
 	return template.TemplateResponse("item.html", {"request": request, "user": user})
 
 
 @app.post("/login")
 def login_to_app(response: Response, credentials: HTTPBasicCredentials = Depends(HTTPBasic())):
 	if credentials.username in app.users and credentials.password == app.users[credentials.username]:
-		s_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret}", encoding='utf8')).hexdigest()
-		app.tokens[s_token] = credentials.username
-		response.set_cookie(key="session_token", value=s_token)
-		print(s_token)
+		session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret}", encoding='utf8')).hexdigest()
+		app.tokens[session_token] = credentials.username
+		response.set_cookie(key="session_token", value=session_token)
+		print(session_token)
 		response.status_code = 307
 		response.headers['Location'] = "/welcome"
 		return response
@@ -60,8 +60,8 @@ def bye_bye(response: Response):
 
 
 @app.post("/patient", status_code=307)
-def add_patient(response: Response, name: str, surname: str, s_token: str = Depends(check_stupid_cookie)):
-	if s_token is None:
+def add_patient(response: Response, name: str, surname: str, session_token: str = Depends(check_stupid_cookie)):
+	if session_token is None:
 		raise HTTPException(status_code=401, detail="dostęp wzbroniony")
 	app.patient_id +=1
 	app.patient_list[app.patient_id] = {name:surname}
@@ -70,22 +70,22 @@ def add_patient(response: Response, name: str, surname: str, s_token: str = Depe
 
 
 @app.get("/patient")
-def show_patients(s_token: str = Depends(check_stupid_cookie)):
-	if s_token is None:
+def show_patients(session_token: str = Depends(check_stupid_cookie)):
+	if session_token is None:
 		raise HTTPException(status_code=401, detail="dostęp wzbroniony")
 	return JSONResponse(app.patient_list)
 
 
 @app.get("/patient/{id}")
-def show_patients(id: int, s_token: str = Depends(check_stupid_cookie)):
-	if s_token is None:
+def show_patients(id: int, session_token: str = Depends(check_stupid_cookie)):
+	if session_token is None:
 		raise HTTPException(status_code=401, detail="dostęp wzbroniony")
 	return JSONResponse(app.patient_list[id])
 
 
 @app.delete("/patient/{id}", status_code=307)
-def show_patients(response: Response, id: int, s_token: str = Depends(check_stupid_cookie)):
-	if s_token is None:
+def show_patients(response: Response, id: int, session_token: str = Depends(check_stupid_cookie)):
+	if session_token is None:
 		raise HTTPException(status_code=401, detail="dostęp wzbroniony")
 	app.patient_list.pop(id)
 	response.headers['Location'] = f"/patient"
